dea00ca58b082df3f0245dbd320735275c0c7d49
jxhsjxhs <976439998@qq.com>
2019-11-29 11:54:23 +0800

    Update sninit.sh

--- sninit.sh
+++ sninit.sh
@@ -5,5 +5,6 @@ yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
 yum -y install docker-ce-18.06.0.ce-3.el7  vim wget  tcpdump nload
 iptables -P FORWARD ACCEPT
+touch /etc/docker/daemon.json
 echo "{"registry-mirrors": ["http://0503026c.m.daocloud.io"]}" > /etc/docker/daemon.json
 systemctl start docker && systemctl enable docker
