25c9978e274b4a10fbccce77e41d430547d5c14b
jxhsjxhs <976439998@qq.com>
2021-01-15 16:30:42 +0800

    Update sninit.sh

--- sninit.sh
+++ sninit.sh
@@ -3,7 +3,7 @@ swapoff -a
 sed -i 's#SELINUX=enforcing#SELINUX=disabled#' /etc/selinux/config
 yum install -y yum-utils device-mapper-persistent-data lvm2
 yum-config-manager --add-repo http://mirrors.aliyun.com/docker-ce/linux/centos/docker-ce.repo
-yum -y install docker-ce  vim wget  tcpdump nload
+yum -y install docker-ce-19.03.9-3.el7  vim wget  tcpdump nload
 iptables -P FORWARD ACCEPT
 systemctl restart docker 
 curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://f1361db2.m.daocloud.io
